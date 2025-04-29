2ba5a8f8afc777a069bf23b706199a904f0847b8
azetino1 <zet16001@byui.edu>
2025-04-29 04:27:18 -0700

    git commit -m 'message'

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        //Prompt the user for their first name
+        Console.WriteLine("What is your first name?");
+        string firstname = Console.ReadLine();
+
+       //Prompt the user for their last name
+        Console.WriteLine("What is your last name?");
+        string lastname = Console.ReadLine();
+
+      //Display fullname  
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
+
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Console.Write("What is the magic number? ");
+        int magicNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("What is your guess? ");
+        int guess = Convert.ToInt32(Console.ReadLine());
+        
+        if (guess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
+
 }
