191dc259bbcf3f9280017d63b60e70f89529eee4
azetino1 <zet16001@byui.edu>
2025-04-30 01:55:24 -0700

    git commit -m 'message'

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -6,15 +6,17 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
 
-                // Prompt the user for a guess
+        // Prompt the user for the magic number
         Console.Write("What is the magic number? ");
         int magicNumber = int.Parse(Console.ReadLine());
 
+        // Initializing with a value that ensures entry into the loop
         int guess = -1;
 
         while (guess != magicNumber)
         {
 
+        //  // Prompt the user for the guess number
         Console.Write("What is your guess? ");
           guess = int.Parse(Console.ReadLine());
 
