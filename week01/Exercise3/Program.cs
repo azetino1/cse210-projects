391a26069f75000453390e2b360084d95ff62584
azetino1 <zet16001@byui.edu>
2025-04-30 01:21:03 -0700

    git commit -m 'message'

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -6,6 +6,33 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
 
+                // Prompt the user for a guess
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+ 
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
         
+        if (guess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
+
+
+
+
+
+
     }
 }
