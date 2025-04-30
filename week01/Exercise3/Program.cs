c4ff492e0157af63ea76de1b8bc93cd954a7a8e4
azetino1 <zet16001@byui.edu>
2025-04-30 01:44:55 -0700

    git commit -m 'message'

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -10,9 +10,13 @@ class Program
         Console.Write("What is the magic number? ");
         int magicNumber = int.Parse(Console.ReadLine());
 
-        
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+
         Console.Write("What is your guess? ");
-         int guess = int.Parse(Console.ReadLine());
+          guess = int.Parse(Console.ReadLine());
 
         
         if (guess < magicNumber)
@@ -27,7 +31,6 @@ class Program
         {
             Console.WriteLine("You guessed it!");
         }
-
     }
-}
-    
+  } 
+}   
