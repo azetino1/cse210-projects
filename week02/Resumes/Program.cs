b48d15ae3fdfcea56a63fb4227b50d1b17c4a70a
azetino1 <zet16001@byui.edu>
2025-05-01 23:48:07 -0700

    commit

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,28 +1,31 @@
 using System;
 
+using System;
+
 class Program
 {
     static void Main(string[] args)
     {
-  
-        // Create first job instance
+        // Create job instances
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
         job1._startYear = 2019;
         job1._endYear = 2022;
 
-        // Display job1 details
-        job1.Display();
-
-        // Create second job instance
         Job job2 = new Job();
-        job2._jobTitle = "Senior Developer";
+        job2._jobTitle = "Manager";
         job2._company = "Apple";
         job2._startYear = 2022;
         job2._endYear = 2023;
 
-        // Display job2 details
-        job2.Display();
+        // Create resume instance
+        Resume myResume = new Resume();
+        myResume._personName = "Allison Rose";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Display full resume
+        myResume.Display();
     }
 }
