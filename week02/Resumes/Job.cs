87fce954f7044b2ff4875755ff13d9fe735afa05
azetino1 <zet16001@byui.edu>
2025-05-01 23:32:49 -0700

    commit

--- week02/Resumes/Job.cs
+++ week02/Resumes/Job.cs
@@ -6,4 +6,11 @@ public class Job
     public string _company;
     public int _startYear;
     public int _endYear;
+
+    // Method to display job details
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+
 }
