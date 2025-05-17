0b6e066b5ff7ca9987bc3af68707bec679b4fb0b
azetino1 <zet16001@byui.edu>
2025-05-17 03:04:02 -0700

    commit

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -140,6 +140,8 @@ class Program
         Order order1 = new Order(customer1);
         order1.AddProduct(new Product("Laptop", 101, 1200m, 1));
         order1.AddProduct(new Product("Mouse", 102, 25m, 2));
+        order1.AddProduct(new Product("Printer", 103, 350m, 3));
+
 
         Order order2 = new Order(customer2);
         order2.AddProduct(new Product("Headphones", 201, 150m, 1));
