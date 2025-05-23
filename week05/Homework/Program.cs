4bc1cef93aad2d7b367c1e4ea9e1648a11787350
azetino1 <zet16001@byui.edu>
2025-05-22 21:59:07 -0700

    commit

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,13 +5,14 @@ class Program
     static void Main(string[] args)
     {
         {
-            MathAssignment math = new MathAssignment("John Doe", "Fractions", "7.3", "3-10, 20-21");
-            WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+         MathAssignment math = new MathAssignment("John April","Fractions", "7.3", "3-10, 20-21");
+        WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
 
-            Console.WriteLine(math.GetSummary());
-            Console.WriteLine(math.GetHomeworkList());
-            Console.WriteLine(writing.GetSummary());
-            Console.WriteLine(writing.GetWritingInfo());
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInfo());
+
         }
     }
 
