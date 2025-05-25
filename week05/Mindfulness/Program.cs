904dad2fc37559a5cc35e5475477e81656cdc5b0
azetino1 <zet16001@byui.edu>
2025-05-24 22:51:02 -0700

    commit

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -18,11 +18,25 @@ class Program
 
             switch (choice)
             {
-                
+                case "1":
+                    BreathingActivity b = new BreathingActivity("Breathing", "This activity will help you relax. Clear your mind, focus on your breathing, and calm your body.");
+                    b.Run();
+                    break;
+                case "2":
+                    ReflectionActivity r = new ReflectionActivity("Reflection", " This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life");
+                    r.Run();
+                    break;
+                case "3":
+                    ListingActivity l = new ListingActivity("Listing", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area");
+                    l.Run();
+                    break;
+                case "4":
+                    running = false;
+                    break;
+
+            }
 
         }
-
-
            
         Console.WriteLine("Thanks for being well with us.");
 
