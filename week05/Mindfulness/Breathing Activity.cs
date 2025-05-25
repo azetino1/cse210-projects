5eb9d50d7eb4ca2461cfea468b074a2690da6fab
azetino1 <zet16001@byui.edu>
2025-05-24 22:58:04 -0700

    commit

--- week05/Mindfulness/Breathing Activity.cs
+++ week05/Mindfulness/Breathing Activity.cs
@@ -1,44 +1,42 @@
 public class BreathingActivity : Activity
-
-public class BreathingActivity : Activity
 {
+
     public BreathingActivity(string name, string description) : base(name, description)
     {
+       
     }
+
     public void Run()
     {
+        DisplayStartingMessage();
+
+        int seconds = 0;
+        while (seconds < _duration)
         {
-            DisplayStartingMessage();
-
-            int seconds = 0;
-            while (seconds < _duration) 
-            {
-                Console.Clear();
-                Console.Write("Breathe in  ");
-                for (int i = 0; i < 4; i++)
-                {
-                    if (seconds >= _duration) { break; } 
-                    Console.Write("."); 
-                    Thread.Sleep(1000);
-                    seconds++;
-                }
-                Console.WriteLine(" ");
-                if (seconds >= _duration) { break; }
-                Console.Write("Breathe out ");
-
-                for (int i = 0; i < 10; i++)
-                {
-                    if (seconds >= _duration) { break; }
-                    Console.Write(".");
-                    Thread.Sleep(1000);
-                    seconds++;
-                }
-            }
-
             Console.Clear();
-            DisplayEndingMessage();
+            Console.Write("Breathe in  ");
+            for (int i = 0; i < 4; i++)
+            {
+                if (seconds >= _duration) { break; } 
+                Console.Write("."); 
+                Thread.Sleep(1000);
+                seconds++;
+            }
+            Console.WriteLine(" ");
+            if (seconds >= _duration) { break; }
+            Console.Write("Breathe out ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (seconds >= _duration) { break; }
+                Console.Write(".");
+                Thread.Sleep(1000);
+                seconds++;
+            }
         }
 
+        Console.Clear();
+        DisplayEndingMessage();
     }
 
 }
