c04cd768e3eb14d6453094a6689aefa5f0e3d960
azetino1 <zet16001@byui.edu>
2025-05-24 22:09:20 -0700

    commit

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -7,7 +7,16 @@ class Program
         bool running = true;
         while (running)
         {
+            Console.Clear();
+            Console.WriteLine("Welcome to the Mindfullness Program. Choose from the following:");
+            Console.WriteLine("1 - Breathing Activity");
+
 
         }
+
+
+           
+        Console.WriteLine("Thanks for being well with us.");
+
     }
 }
