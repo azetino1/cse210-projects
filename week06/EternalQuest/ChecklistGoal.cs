53b280365268b879b5c1bdb01530a60cbfc45a5b
azetino1 <zet16001@byui.edu>
2025-05-29 21:12:25 -0700

    commit

--- week06/EternalQuest/ChecklistGoal.cs
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -18,3 +18,19 @@ public class ChecklistGoal : Goal
         }
         return true;
     }
+     public override int RecordEvent()
+    {
+        //This is an Checklist Goal. It is. Add one to the counter, then return the points
+        _amountCompleted++;
+        if(_amountCompleted == _target){
+            return _points + _bonus; //If you hit the target, return the points + the bonus instead
+        }
+        return _points;
+    }
+
+    public override string GetStringRepresentation() {
+        return $"Goal: {_name}\nDescription: {_description}\nType: Checklist\nCompleted:{IsComplete()}";
+    }
+
+    
+}
