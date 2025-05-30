55f5d44439edfaacdd4b6c61629dec6387848d0a
azetino1 <zet16001@byui.edu>
2025-05-29 22:47:48 -0700

    commit

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -6,12 +6,12 @@ class Program
     {
         GoalManager manager = new GoalManager();
 
-        // Adding goals
+        
         manager.AddGoal(new SimpleGoal("Run a Marathon", 1000));
         manager.AddGoal(new EternalGoal("Read Scriptures", 100));
         manager.AddGoal(new ChecklistGoal("Attend Temple", 50, 10, 500));
 
-        // Display and interact
+     
         manager.DisplayGoals();
         manager.RecordGoalProgress("Read Scriptures");
 
