249f0c1c78881251bc37d0d7c287a9e54b9d04e6
azetino1 <zet16001@byui.edu>
2025-05-29 21:20:44 -0700

    commit

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,12 @@
+public abstract class Goal
+{
+    protected string _name;
+    protected string _description;
+    protected int _points;
+
+    public Goal(string name, string desc, int points)
+    {
+        _name = name;
+        _description = desc;
+        _points = points; 
+    }
