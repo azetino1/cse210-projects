070b82e53aac78aa69ea7650972cd1a7bcb86116
azetino1 <zet16001@byui.edu>
2025-05-29 20:41:00 -0700

    commit

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -6,6 +6,10 @@ class Program
     {
         List<Shape> shapes = new List<Shape>();
         shapes.Add(new Rectangle("Red", 2, 4));
+        shapes.Add(new Square("Green", 2));
+        shapes.Add(new Circle("Blue", 2));
+
+        foreach (Shape s in shapes)
 
         {
             Console.WriteLine($"Color: {s.GetColor()}, Area: {s.GetArea()}");
