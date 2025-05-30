62fe805dbb2c3558b58bb7fc240156ed0e3dfcd2
azetino1 <zet16001@byui.edu>
2025-05-29 21:17:02 -0700

    commit

--- week06/EternalQuest/EternalGoal.cs
+++ week06/EternalQuest/EternalGoal.cs
@@ -4,4 +4,25 @@ public class EternalGoal : Goal
 
     public EternalGoal(string name, string desc, int points) : base(name, desc, points)
     {
+        
     }
+    
+    public override bool IsComplete()
+    {
+        return false;
+    }
+
+    public override int RecordEvent()
+    {
+        
+        _count++;
+        return _points;
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"Goal: {_name}\nDescription: {_description}\nType: Eternal\nCompleted:{_count} Times";
+    }
+
+    
+}
