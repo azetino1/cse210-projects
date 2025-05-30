62ee32cd75bf8d729d1ba2f9f66af91fc1ae0754
azetino1 <zet16001@byui.edu>
2025-05-29 21:13:57 -0700

    commit

--- week06/EternalQuest/ChecklistGoal.cs
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -1,8 +1,8 @@
 public class ChecklistGoal : Goal
 {
-    private int _amountCompleted; // How many times the Checklist Goal was completed
-    private int _target; // How many times do you want to complete this goal?
-    private int _bonus; //If you hit the target, bonus moment.
+    private int _amountCompleted; 
+    private int _target; 
+    private int _bonus; 
 
     public ChecklistGoal(string name, string desc, int points, int target, int bonus) : base(name, desc, points)
     {
@@ -20,10 +20,10 @@ public class ChecklistGoal : Goal
     }
      public override int RecordEvent()
     {
-        //This is an Checklist Goal. It is. Add one to the counter, then return the points
+       
         _amountCompleted++;
         if(_amountCompleted == _target){
-            return _points + _bonus; //If you hit the target, return the points + the bonus instead
+            return _points + _bonus; 
         }
         return _points;
     }
---
+++ week06/EternalQuest/EternalGoal.cs
@@ -0,0 +1,8 @@
+public class EternalGoal : Goal
+{
+    private int _count; // How many times the Eternal Goal was completed
+
+    public EternalGoal(string name, string desc, int points) : base(name, desc, points)
+    {
+        //Constructor is identical to the base Goal class
+    }
