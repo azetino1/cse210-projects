6bf3811958d3279959cbf3df787d4d54b679ebcc
azetino1 <zet16001@byui.edu>
2025-05-29 21:14:55 -0700

    commit

--- week06/EternalQuest/EternalGoal.cs
+++ week06/EternalQuest/EternalGoal.cs
@@ -1,8 +1,7 @@
 public class EternalGoal : Goal
 {
-    private int _count; // How many times the Eternal Goal was completed
+    private int _count;
 
     public EternalGoal(string name, string desc, int points) : base(name, desc, points)
     {
-        //Constructor is identical to the base Goal class
     }
