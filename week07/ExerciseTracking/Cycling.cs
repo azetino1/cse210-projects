812753ab1de2cbfec9f95d1a2a712c6e12bfc3e5
azetino1 <zet16001@byui.edu>
2025-06-03 00:00:06 -0700

    commit

---
+++ week07/ExerciseTracking/Cycling.cs
@@ -0,0 +1,13 @@
+class Cycling : Activity
+{
+    private double Speed { get; }
+
+    public Cycling(DateTime date, int duration, double speed) : base(date, duration)
+    {
+        Speed = speed;
+    }
+
+    public override double GetDistance() => (Speed * Duration) / 60;
+    public override double GetSpeed() => Speed;
+    public override double GetPace() => 60 / Speed;
+}
