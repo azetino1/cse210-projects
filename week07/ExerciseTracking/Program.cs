7af1e5f7e1611180706a3f9311632ba82928e369
azetino1 <zet16001@byui.edu>
2025-06-03 00:13:31 -0700

    commit

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
-    }
+        List<Activity> activities = new List<Activity>
+        }
+        new Running(new DateTime(2025, 6, 2), 30, 3.0)
+        new Cycling(new DateTime(2025, 6, 2), 45, 15.0),
+        new Swimming(new DateTime(2025, 6, 2), 20, 10)
+        };
+
+
+
 }
