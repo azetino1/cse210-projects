6c57e76c6ce8c1a6d0270e52cc9d0fe1cb10e5d7
azetino1 <zet16001@byui.edu>
2025-06-03 00:18:58 -0700

    commit

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         List<Activity> activities = new List<Activity>
-        }
+        {
         new Running(new DateTime(2025, 6, 2), 30, 3.0)
         new Cycling(new DateTime(2025, 6, 2), 45, 15.0),
         new Swimming(new DateTime(2025, 6, 2), 20, 10)
